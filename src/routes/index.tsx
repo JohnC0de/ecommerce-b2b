d9c885e6ae067a98a3889bcf11ad7c2c32d8ddd4
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-22 13:58:09 -0300

    feat: placeholder home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,18 @@
 import { Title } from "solid-start"
-import Counter from "~/components/Counter"
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1 class="bg-red-500">Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>E-Commerce B2B</Title>
+      <div class="flex flex-col items-center justify-center min-h-screen gap-4">
+        <h1 class="text-6xl font-bold">E-Commerce B2B</h1>
+        <div class="flex flex-col items-center gap-4">
+          <p class="text-center text-xl text-gray-500 sm:w-96">Coming soon...</p>
+          <a href="/login" class="text-lg p-4 text-white bg-blue-500 rounded-lg shadow-xl hover:bg-blue-700">
+            Login
+          </a>
+        </div>
+      </div>
     </main>
   )
 }
