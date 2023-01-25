150e26d19e5c817dc60bd644394b0334c82d311f
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-25 14:17:25 -0300

    chore: update depts, disable ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import solid from "solid-start/vite"
 import UnoCSS from "unocss/vite"
 import { defineConfig } from "vite"
+// @ts-expect-error no typing
+// import vercel from "solid-start-vercel"
 
 export default defineConfig({
-  plugins: [solid({ ssr: true }), UnoCSS()]
+  plugins: [
+    solid({
+      ssr: false
+      // adapter: vercel({ edge: false })
+    }),
+    UnoCSS()
+  ]
 })
