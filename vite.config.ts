19625affdbc451c4bdc911ef67b394989cc0c3cd
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-26 01:11:38 -0300

    chore: disable tranformerVariants

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import solid from "solid-start/vite"
 import UnoCSS from "unocss/vite"
 import { defineConfig } from "vite"
-// @ts-expect-error no typing
-// import vercel from "solid-start-vercel"
 
 export default defineConfig({
-  plugins: [
-    solid({
-      ssr: false
-      // adapter: vercel({ edge: false })
-    }),
-    UnoCSS()
-  ]
+  plugins: [solid({ ssr: false }), UnoCSS({})]
 })
